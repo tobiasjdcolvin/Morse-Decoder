493b5a01c83961d4f9948ab9311ee48f89a125f6
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2024-06-08 00:14:18 -0700

    Created code to later apply to a BST, added some styling.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const codeViewBox = document.getElementById("codeViewBox");
+const resultViewBox = document.getElementById("resultViewBox");
+let inputText;
+let outputText = "";
 
 function dotClicked() {
     codeViewBox.textContent += ".";
@@ -8,10 +11,38 @@ function dashClicked() {
     codeViewBox.textContent += "-";
 }
 
-function spaceClicked() {
+function newLetterClicked() {
     codeViewBox.textContent += " ";
 }
 
 function clearClicked() {
     codeViewBox.textContent = "";
+    resultViewBox.textContent = "";
+    outputText = "";
+}
+
+function newWordClicked() {
+    codeViewBox.textContent += "\t";
+}
+
+function decodeClicked() {
+    // adds a new letter character " " so as to print the current character.
+    codeViewBox.textContent += " ";
+    inputText = codeViewBox.textContent;
+
+    for (let i = 0; i < inputText.length; i++) {
+        let curr = inputText.charAt(i);
+        if (curr == ".") {
+            outputText += "right\n";
+        } else if (curr == "-") {
+            outputText += "left\n";
+        } else if (curr == " ") {
+            outputText += "re-root, PRINT LETTER\n";
+        } else if (curr == "\t") {
+            outputText += "re-root, PRINT LETTER + SPACE\n";
+        }
+    }
+
+    resultViewBox.textContent = outputText;
+
 }
